2750067ff43e34b60bd7e985098f75d1202604c7
Matt <me@mattjones.tech>
2020-02-05 15:18:25 -0500

    add orbit controls

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,7 +1,12 @@
 // import * as THREE from '/node_modules/three/build/three.module.js';
 
-var camera, scene, renderer;
+var camera, renderer;
 var geometry, material, mesh;
+var renderer = new THREE.WebGLRenderer( { antialias: true } );
+var loader = new THREE.GLTFLoader();
+var controls = new THREE.OrbitControls(camera, renderer.domElement);
+var light = new THREE.PointLight(0xFFFFFF, 1);
+var scene = new THREE.Scene();
 
 
 init();
@@ -10,9 +15,11 @@ animate();
 function init() {
 
     camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
+
     camera.position.z = 3;
 
-    scene = new THREE.Scene();
+
+
 
     // geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
     // material = new THREE.MeshNormalMaterial();
@@ -20,21 +27,23 @@ function init() {
     // mesh = new THREE.Mesh( geometry, material );
     // scene.add( mesh );
 
-    var light = new THREE.PointLight(0xFFFFFF, 1);
+
     light.position.set(0, 10, 0);
     scene.add(light);
 
-    var loader = new THREE.GLTFLoader();
+
     loader.load('/3dobjects/suzanne.glb', function(gltf) {
       scene.add(gltf.scene);
     }, undefined, function(error) {
       console.error(error);
     });
 
-    renderer = new THREE.WebGLRenderer( { antialias: true } );
+
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
 
+
+    controls.update();
 }
 
 function animate() {
@@ -44,5 +53,6 @@ function animate() {
     // mesh.rotation.x += 0.01;
     // mesh.rotation.y += 0.05;
 
+    controls.update();
     renderer.render( scene, camera );
 };
