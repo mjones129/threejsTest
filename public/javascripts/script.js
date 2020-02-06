ebe67b05d72626eafbe31adf6dd90e38309706d2
Matt <me@mattjones.tech>
2020-02-06 12:46:11 -0500

    redraw page on resize

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,12 +1,8 @@
+
 // import * as THREE from '/node_modules/three/build/three.module.js';
 
-var camera, renderer;
+var camera, scene, renderer;
 var geometry, material, mesh;
-var renderer = new THREE.WebGLRenderer( { antialias: true } );
-var loader = new THREE.GLTFLoader();
-var controls = new THREE.OrbitControls(camera, renderer.domElement);
-var light = new THREE.PointLight(0xFFFFFF, 1);
-var scene = new THREE.Scene();
 
 
 init();
@@ -15,11 +11,9 @@ animate();
 function init() {
 
     camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
-
     camera.position.z = 3;
 
-
-
+    scene = new THREE.Scene();
 
     // geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
     // material = new THREE.MeshNormalMaterial();
@@ -27,23 +21,29 @@ function init() {
     // mesh = new THREE.Mesh( geometry, material );
     // scene.add( mesh );
 
-
+    var light = new THREE.PointLight(0xFFFFFF, 1);
     light.position.set(0, 10, 0);
     scene.add(light);
 
-
+    var loader = new THREE.GLTFLoader();
     loader.load('/3dobjects/suzanne.glb', function(gltf) {
       scene.add(gltf.scene);
     }, undefined, function(error) {
       console.error(error);
     });
 
-
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
 
+    window.addEventListener('resize', function() {
+      var width = window.innerWidth;
+      var height = window.innerHeight;
+      renderer.setSize(width, height);
+      camera.aspect = width / height;
+      camera.updateProjectionMatrix();
+    });
 
-    controls.update();
 }
 
 function animate() {
@@ -53,6 +53,5 @@ function animate() {
     // mesh.rotation.x += 0.01;
     // mesh.rotation.y += 0.05;
 
-    controls.update();
     renderer.render( scene, camera );
 };
